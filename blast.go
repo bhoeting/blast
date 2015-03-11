edebfe4cd5bc2de812f59da2e71904d4c8a6171a
Brennan Hoeting <brennan.hoeting@gmail.com>
2015-03-11 17:44:18 -0400

    Finalize generic parsing

--- blast.go
+++ blast.go
@@ -33,16 +33,69 @@ func (b *Blast) ParseLine(code string) string {
 // returns its value as a string
 func (b *Blast) ParseGeneric(code string) string {
 	tokens := NewTokenArray(code)
+	positions := getOperatorPositions(tokens)
+	lenTokens := len(tokens)
+	lenPositions := len(positions)
 
-	if len(tokens) == 1 {
-		return tokens[0].String()
-	}
+	for lenPositions > 0 {
+		pos := positions[0]
+		tokens[pos-1] = HandleTokens(tokens[pos-1], tokens[pos+1], tokens[pos], b)
 
-	for index, token := range tokens {
-		if op := token.Op(); op != -1 {
-			return HandleTokens(tokens[index-1], tokens[index+1], token, b).String()
+		for tIndex := pos; tIndex < lenTokens-2; tIndex++ {
+			tokens[tIndex] = tokens[tIndex+2]
+		}
+
+		tokens[lenTokens-1] = new(Token)
+		tokens[lenTokens-2] = new(Token)
+		tokens = tokens[:lenTokens-2]
+		lenTokens -= 2
+
+		copy(positions[0:], positions[1:])
+		positions[len(positions)-1] = 0
+		positions = positions[:len(positions)-1]
+		lenPositions -= 1
+
+		for pIndex := 0; pIndex < lenPositions; pIndex++ {
+			if positions[pIndex] > pos {
+				positions[pIndex] -= 2
+			}
 		}
 	}
 
-	return ""
+	return tokens[0].String()
+}
+
+// getOperatorPositions returns an array of
+// operator indicies sorted by the order
+// in which their operands are to be evaluated.
+func getOperatorPositions(tokens []*Token) []int {
+	multAndDiv, addAndSub, strs :=
+		make([]int, 0), make([]int, 0), make([]int, 0)
+
+	// Loop through each token, but only act on operators.
+	for index, token := range tokens {
+		if token.Type() == TokenTypeOperator {
+			// If either of the operator's operands is a
+			// string, add the index to the strs array.
+			if tokens[index-1].Type() == TokenTypeString ||
+				tokens[index+1].Type() == TokenTypeString {
+				strs = append(strs, index)
+				continue
+			}
+
+			// If the operator is addition/subtraction,
+			// send it to the addAndSub slice.
+			if token.Op() == OpTypeAddition ||
+				token.Op() == OpTypeSubtraction {
+				addAndSub = append(addAndSub, index)
+			} else {
+				// Otherwise, it must be multiplication/division.
+				// send it to the multAndDiv slice.
+				multAndDiv = append(multAndDiv, index)
+			}
+		}
+	}
+
+	// Combine the slices in the order of their precedence.
+	return append(append(multAndDiv, addAndSub...), strs...)
 }
--- blast_test.go
+++ blast_test.go
@@ -1,6 +1,9 @@
 package blast
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestGenericParser(t *testing.T) {
 	b := new(Blast)
@@ -12,5 +15,22 @@ func TestGenericParser(t *testing.T) {
 	r = b.ParseGeneric("3 * \"lol\"")
 	if r != "lollollol" {
 		t.Fatalf("Failed to parse generic code, wanted %v, got %v", "lollollol", r)
+	}
+
+	r = b.ParseGeneric("3 * 8 + 3 / 4 * \"derp\"")
+	if r != strings.Repeat("derp", 24) {
+		t.Fatalf("Failed to parse generic code, wanted %v, got %v", strings.Repeat("derp", 24), r)
+	}
+}
+
+func TestPositions(t *testing.T) {
+	positions := getOperatorPositions(NewTokenArray("2 + 2 * 3 - 1 / 3"))
+
+	if len(positions) != 4 {
+		t.Fatalf("Failed to get operator positions array length, expected %v, got %v, %v", 4, len(positions), positions)
+	}
+
+	if positions[0] != 3 || positions[1] != 7 || positions[2] != 1 || positions[3] != 5 {
+		t.Fatal("Failed to get correct operator positions %v", positions)
 	}
 }
