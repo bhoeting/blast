d128abdd61ce2c1a490210dd88978b4017fd3805
Brennan Hoeting <brennan.hoeting@gmail.com>
2015-03-14 22:49:06 -0400

    Add token stream

--- token.go
+++ token.go
@@ -1,20 +1,31 @@
 package blast
 
-import "fmt"
-
-const (
-	tokenTypeOp    = 3
-	tokenTypeChar  = 2
-	tokenTypeParen = 1
-	tokenTypeQuote = 4
-	tokenTypeSpace = 5
+import (
+	"fmt"
+	"strconv"
+	"unicode"
 )
 
 const (
-	opTypeAddition       = 1
-	opTypeSubtraction    = 2
-	opTypeMultiplication = 3
-	opTypeDivision       = 4
+	tokenTypeUnknown = -1
+	tokenTypeOp      = 3
+	tokenTypeChar    = 2
+	tokenTypeParen   = 1
+	tokenTypeQuote   = 4
+	tokenTypeSpace   = 5
+	tokenTypeString  = 6
+	tokenTypeInt     = 7
+	tokenTypeFloat   = 8
+	tokenTypeVar     = 9
+)
+
+type opType int
+
+const (
+	opTypeAddition opType = iota
+	opTypeSubtraction
+	opTypeMultiplication
+	opTypeDivision
 )
 
 const (
@@ -27,6 +38,7 @@ const (
 	spaceIdentifier          = " "
 	openParenIdentifier      = "("
 	closeParenIdentifier     = ")"
+	decimalPointIdentifier   = "."
 )
 
 const (
@@ -39,8 +51,111 @@ type token struct {
 	t    int
 }
 
+type tokenStream struct {
+	tokens []*token
+	size   int
+	index  int
+}
+
+func newTokenStream(strTokens string) *tokenStream {
+	ts := new(tokenStream)
+	ts.tokens = parseTokens(strTokens)
+	ts.size = len(ts.tokens)
+	ts.index = 0
+	return ts
+}
+
+func (ts *tokenStream) next() (*token, bool) {
+	if ts.index > ts.size-1 {
+		return new(token), false
+	}
+
+	tok := ts.curr()
+	ts.index++
+
+	return tok, true
+}
+
+func (ts *tokenStream) combine() *tokenStream {
+	replacementTokens := make([]*token, 0)
+	tokensToBeCombined := make([]*token, 0)
+	prevTokensEndIndex := 0
+
+	ts.each(func(token *token, index int) {
+		if token.t != tokenTypeOp || token.t != tokenTypeSpace {
+			tokensToBeCombined = append(tokensToBeCombined, token)
+		}
+
+		if token.t == tokenTypeOp || index == ts.size-1 {
+			replacementTokens = append(replacementTokens,
+				combineTokens(tokensToBeCombined[prevTokensEndIndex:]))
+
+			prevTokensEndIndex = len(tokensToBeCombined)
+			return
+		}
+	})
+
+	ts.tokens = replacementTokens
+	ts.size = len(replacementTokens) - 1
+
+	return ts
+}
+
+func combineTokens(tokens []*token) *token {
+	strCombinedTok := ""
+	isNum, isDecimal, isStr := true, false, false
+
+	if tokens[0].t == tokenTypeQuote &&
+		tokens[len(tokens)-1].t == tokenTypeQuote {
+		isStr = true
+	}
+
+	for _, t := range tokens {
+		if strTok, ok := t.data.(string); ok {
+			strCombinedTok += strTok
+			if !unicode.IsNumber(rune(strTok[0])) {
+				if strTok != subtractionIdentifier {
+					if strTok != decimalPointIdentifier {
+						isNum = false
+					} else {
+						isDecimal = true
+					}
+				}
+			}
+		}
+	}
+
+	if isStr {
+		return newToken(strCombinedTok, tokenTypeString)
+	}
+
+	if isNum {
+		if isDecimal {
+			f, _ := strconv.ParseFloat(strCombinedTok, 64)
+			return newToken(f, tokenTypeFloat)
+		} else {
+			i, _ := strconv.Atoi(strCombinedTok)
+			return newToken(i, tokenTypeInt)
+		}
+	}
+
+	return newToken(strCombinedTok, tokenTypeVar)
+}
+
+// each is a helper method that runs a function
+// on each of the tokens in tokenStream's token slice
+func (ts *tokenStream) each(f func(tok *token, index int)) {
+	for next, ok := ts.next(); ok == true; next, ok = ts.next() {
+		f(next, ts.index-1)
+	}
+}
+
+func (ts *tokenStream) curr() *token {
+	return ts.tokens[ts.index]
+}
+
 func (t *token) string() string {
-	return fmt.Sprintf("%v", data)
+	return fmt.Sprintf("%v", t.data)
 }
 
 func newToken(data interface{}, t int) *token {
@@ -84,7 +199,7 @@ func parseToken(strToken string) *token {
 	return newToken(strToken, tokenTypeChar)
 }
 
-func parseOperator(strToken string) int {
+func parseOperator(strToken string) opType {
 	switch strToken {
 	case additionIdentifier:
 		return opTypeAddition
--- token_test.go
+++ token_test.go
@@ -2,9 +2,13 @@ package blast
 
 import "testing"
 
-func TestTokenGetter(t *testing.T) {
-	tokens := parseTokens("2+(2-9) + \"")
+// testTokenStream tests that the correct
+// token type is created for each token
+// in the test string
+func TestTokenStream(t *testing.T) {
+	ts := newTokenStream("2+(2-9) + \"")
 
+	// The desired token types
 	tokenTypes := []int{
 		tokenTypeChar, tokenTypeOp,
 		tokenTypeParen, tokenTypeChar,
@@ -14,13 +18,32 @@ func TestTokenGetter(t *testing.T) {
 		tokenTypeQuote,
 	}
 
-	for i, _ := range tokenTypes {
-		if tokenTypes[i] != tokens[i].t {
-			t.Fatalf(
-				`Failed to get correct token types, 
-				 got %v, expected %v, in index %v`,
-				tokens[i].t, tokenTypes[i], i,
-			)
+	// Check that each token matches the
+	// desired token types
+	ts.each(func(tok *token, index int) {
+		if tokenTypes[index] != tok.t {
+			t.Fatalf("Could not get correct token types, index: %v want: %v got: %v",
+				ts.index, tokenTypes[index], tok.t)
 		}
+	})
+}
+
+func TestCombiningOfTokens(t *testing.T) {
+	ts := newTokenStream("23+\"string\"+300.2").combine()
+
+	firstToken := ts.tokens[0].data.(int)
+	secondToken := ts.tokens[1].data.(string)
+	thirdToken := ts.tokens[2].data.(float64)
+
+	if firstToken != 23 {
+		t.Fatalf("Could not combine tokens, wanted %v got %v", 21, firstToken)
+	}
+
+	if secondToken != "string" {
+		t.Fatalf("Could not combine tokens, wanted %v got %v", "string", secondToken)
+	}
+
+	if thirdToken != 300.2 {
+		t.Fatalf("Could not combine tokens, wanted %v got %v", 300.2, thirdToken)
 	}
 }
