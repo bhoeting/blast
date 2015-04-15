0b3f70868ef992055fa43c5709669eeeb2caabca
Brennan <brennan.hoeting@gmail.com>
2015-04-15 10:45:05 -0400

    Add new lexer

---
+++ lexer.go
@@ -0,0 +1,358 @@
+package blast
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+type Lexer struct {
+	pos        int
+	width      int
+	parenDepth int
+	itemPos    int
+	text       string
+	curr       string
+	items      []*Item
+	state      lexerState
+}
+
+type lexerState int
+
+type lexerFn func(l *Lexer) lexerFn
+
+const (
+	lexerStateInQuotes = iota
+	lexerStateDefault
+)
+
+type Item struct {
+	typ  itemType
+	pos  int
+	text string
+}
+
+func NewItem(text string, pos int, typ itemType) *Item {
+	item := new(Item)
+	item.pos = pos
+	item.text = text
+	item.typ = typ
+	return item
+}
+
+func (i *Item) String() string {
+	return fmt.Sprintf("%s", i.text)
+}
+
+type itemType int
+
+const (
+	itemTypeEOF = iota
+	itemTypeNum
+	itemTypeBool
+	itemTypeString
+	itemTypeOperator
+	itemTypeIdentifier
+	itemTypeOpenParen
+	itemTypeCloseParen
+	itemTypeComma
+	itemTypeIf
+	itemTypeFunction
+	itemTypeElse
+	itemTypeReturn
+	itemTypeEnd
+)
+
+func (typ itemType) String() string {
+	switch typ {
+	case itemTypeNum:
+		return "Number"
+	case itemTypeBool:
+		return "Bool"
+	case itemTypeString:
+		return "String"
+	case itemTypeOperator:
+		return "Operator"
+	case itemTypeComma:
+		return "Comma"
+	case itemTypeOpenParen:
+		return "Open paren"
+	case itemTypeCloseParen:
+		return "Close paren"
+	case itemTypeIdentifier:
+		return "Identifier"
+	}
+
+	return "Unknown"
+}
+
+var (
+	itemEOF = &Item{
+		typ:  itemTypeEOF,
+		pos:  -1,
+		text: "<NULL>",
+	}
+
+	reservedKey = map[string]itemType{
+		"if":       itemTypeIf,
+		"else":     itemTypeElse,
+		"true":     itemTypeBool,
+		"false":    itemTypeBool,
+		"return":   itemTypeReturn,
+		"function": itemTypeFunction,
+		"end":      itemTypeEnd,
+	}
+)
+
+const eof = -1
+
+func NewLexer(text string) *Lexer {
+	lexer := new(Lexer)
+	lexer.state = lexerStateDefault
+	lexer.text = text
+	return lexer
+}
+
+func (l *Lexer) String() string {
+	str := ""
+
+	for _, item := range l.items {
+		str += item.String()
+	}
+
+	return str
+}
+
+func (l *Lexer) Lex() lexerFn {
+
+	for l.HasNext() {
+		r := l.Peek()
+
+		if r == '"' {
+			return l.LexString()
+		}
+
+		if unicode.IsSpace(r) {
+			l.Next()
+			return l.Lex()
+		}
+
+		switch r {
+		case '(':
+			l.Consume(l.Next())
+			l.PushItem(itemTypeOpenParen)
+			return l.Lex()
+		case ')':
+			l.Consume(l.Next())
+			l.PushItem(itemTypeCloseParen)
+			return l.Lex()
+		case ',':
+			l.Consume(l.Next())
+			l.PushItem(itemTypeComma)
+			return l.Lex()
+		}
+
+		if isAlphaNumeric(r) {
+			return l.LexIdentifier()
+		}
+
+		if r == '.' || r == '-' || unicode.IsNumber(r) {
+			return l.LexNumber()
+		}
+
+		if isOperatorPiece(r) {
+			return l.LexOperator()
+		}
+	}
+
+	return l.Stop()
+}
+
+func (l *Lexer) LexIdentifier() lexerFn {
+	var next rune
+
+	l.ConsumeUntilNotValid(func(r rune) bool {
+		next = r
+		return isAlphaNumeric(r) || r == '.'
+	})
+
+	_, err := strconv.ParseFloat(l.curr, 64)
+
+	if err == nil {
+		return l.LexNumber()
+	}
+
+	l.PushItem(parseItemTypeFromString(l.curr))
+	return l.Lex()
+}
+
+func (l *Lexer) LexNumber() lexerFn {
+	l.ConsumeUntilNotValid(func(r rune) bool {
+
+		// Check that a negative sign
+		// only occurs at the beginning
+		if r == '-' {
+			return len(l.curr) == 0
+		}
+
+		// Check that only one decimal point
+		// occurs in the string
+		if r == '.' {
+			if strings.ContainsRune(l.curr, '.') {
+				l.Errorf("Too many decimal points in %s", l.curr+string(r))
+			} else {
+				return true
+			}
+		}
+
+		// Check the rune is a number
+		return unicode.IsNumber(r)
+	})
+
+	l.PushItem(itemTypeNum)
+	return l.Lex()
+}
+
+func (l *Lexer) LexOperator() lexerFn {
+	l.ConsumeUntilNotValid(func(r rune) bool {
+		return isOperatorPiece(r)
+	})
+
+	if !isOperator(l.curr) {
+		l.Errorf("Invalid operator %s", l.curr)
+	}
+
+	l.PushItem(itemTypeOperator)
+	return l.Lex()
+}
+
+func (l *Lexer) LexString() lexerFn {
+	l.Next()
+	l.ConsumeUntilNotValid(func(r rune) bool {
+		return r != '"'
+	})
+
+	l.PushItem(itemTypeString)
+	l.Next()
+	return l.Lex()
+}
+
+func (l *Lexer) ConsumeUntilNotValid(isValid func(r rune) bool) {
+	for r := l.Next(); isValid(r); r = l.Next() {
+		l.Consume(r)
+	}
+
+	if l.HasNext() {
+		l.Backup()
+	}
+}
+
+func (l *Lexer) Stop() lexerFn {
+	l.curr = ""
+	l.pos = -1
+	return nil
+}
+
+func (l *Lexer) PushItem(typ itemType) *Lexer {
+	item := NewItem(l.curr, l.pos, typ)
+	l.items = append(l.items, item)
+	l.curr = ""
+	return l
+}
+
+func (l *Lexer) AtTerminator() bool {
+	r := l.Peek()
+
+	if unicode.IsSpace(r) {
+		return true
+	}
+
+	switch r {
+	case eof, ',', '(', ')':
+		return true
+	}
+
+	return false
+}
+
+func (l *Lexer) Next() rune {
+	if !l.HasNext() {
+		return eof
+	}
+
+	r, width := utf8.DecodeRuneInString(l.text[l.pos:])
+	l.pos += width
+	l.width = width
+	return r
+}
+
+func (l *Lexer) HasNext() bool {
+	return l.pos < len(l.text)
+}
+
+func (l *Lexer) Peek() rune {
+	r := l.Next()
+	l.Backup()
+	return r
+}
+
+func (l *Lexer) Backup() *Lexer {
+	l.pos -= l.width
+	return l
+}
+
+func (l *Lexer) Consume(r rune) *Lexer {
+	l.curr += string(r)
+	return l
+}
+
+func (l *Lexer) Flush() *Lexer {
+	l.curr = ""
+	return l
+}
+
+func (l *Lexer) NextItem() *Item {
+	if !l.HasNextItem() {
+		return itemEOF
+	}
+
+	item := l.items[l.itemPos]
+	l.itemPos++
+	return item
+}
+
+func (l *Lexer) HasNextItem() bool {
+	return l.itemPos < len(l.items)
+}
+
+func (l *Lexer) Errorf(errFmt string, args ...interface{}) {
+	log.Fatalf("Blast error at pos "+string(l.pos)+": "+errFmt, args...)
+}
+
+func parseItemTypeFromString(text string) itemType {
+	if typ, ok := reservedKey[text]; ok {
+		return typ
+	}
+
+	return itemTypeIdentifier
+}
+
+func isOperator(strOp string) bool {
+	return strOp == "+" || strOp == "-" || strOp == "*" ||
+		strOp == "/" || strOp == "^" || strOp == "&&" || strOp == "||"
+}
+
+func isOperatorPiece(r rune) bool {
+	return r == '+' || r == '-' || r == '*' || r == '/' || r == '^' || r == '&' || r == '|'
+}
+
+func isAlphaNumeric(r rune) bool {
+	return r == '_' || unicode.IsNumber(r) || unicode.IsLetter(r)
+}
+
+func isAlpha(r rune) bool {
+	return r == '_' || unicode.IsLetter(r)
+}
---
+++ lexer_test.go
@@ -0,0 +1,93 @@
+package blast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLexerMovement(t *testing.T) {
+	lexer := NewLexer("200+\n")
+
+	assert.Equal(t, '2', lexer.Next())
+	assert.Equal(t, '0', lexer.Next())
+	assert.Equal(t, '0', lexer.Next())
+
+	lexer.Backup()
+
+	assert.Equal(t, '0', lexer.Next())
+	assert.Equal(t, '+', lexer.Peek())
+	assert.Equal(t, '+', lexer.Next())
+	assert.Equal(t, '\n', lexer.Next())
+	assert.Equal(t, eof, lexer.Next())
+}
+
+func TestLexerItemText(t *testing.T) {
+	lexer := NewLexer("201+901")
+	lexer.Lex()
+	assert.Equal(t, "201", lexer.NextItem().text)
+	assert.Equal(t, "+", lexer.NextItem().text)
+	assert.Equal(t, "901", lexer.NextItem().text)
+
+	lexer = NewLexer("x2,x1,91,-99.9,-.78 .87")
+	lexer.Lex()
+	assert.Equal(t, "x2", lexer.NextItem().text)
+	assert.Equal(t, ",", lexer.NextItem().text)
+	assert.Equal(t, "x1", lexer.NextItem().text)
+	assert.Equal(t, ",", lexer.NextItem().text)
+	assert.Equal(t, "91", lexer.NextItem().text)
+	assert.Equal(t, ",", lexer.NextItem().text)
+	assert.Equal(t, "-99.9", lexer.NextItem().text)
+	assert.Equal(t, ",", lexer.NextItem().text)
+	assert.Equal(t, "-.78", lexer.NextItem().text)
+	assert.Equal(t, ".87", lexer.NextItem().text)
+
+	lexer = NewLexer("300 - -30")
+	lexer.Lex()
+	assert.Equal(t, "300", lexer.NextItem().text)
+	assert.Equal(t, "-", lexer.NextItem().text)
+	assert.Equal(t, "-30", lexer.NextItem().text)
+
+	lexer = NewLexer("314.9 + \"str()_=182ing\"")
+	lexer.Lex()
+	assert.Equal(t, "314.9", lexer.NextItem().text)
+	assert.Equal(t, "+", lexer.NextItem().text)
+	assert.Equal(t, "str()_=182ing", lexer.NextItem().text)
+}
+
+func TestLexerItemType(t *testing.T) {
+	lexer := NewLexer("200 200.89 -.99 && xx || \"derp\" (),")
+	lexer.Lex()
+
+	assertItemType(t, itemTypeNum, lexer.NextItem())
+	assertItemType(t, itemTypeNum, lexer.NextItem())
+	assertItemType(t, itemTypeNum, lexer.NextItem())
+	assertItemType(t, itemTypeOperator, lexer.NextItem())
+	assertItemType(t, itemTypeIdentifier, lexer.NextItem())
+	assertItemType(t, itemTypeOperator, lexer.NextItem())
+	assertItemType(t, itemTypeString, lexer.NextItem())
+	assertItemType(t, itemTypeOpenParen, lexer.NextItem())
+	assertItemType(t, itemTypeCloseParen, lexer.NextItem())
+	assertItemType(t, itemTypeComma, lexer.NextItem())
+}
+
+func TestReservedItems(t *testing.T) {
+	lexer := NewLexer("return if else end function XX")
+	lexer.Lex()
+
+	assertItemType(t, itemTypeReturn, lexer.NextItem())
+	assertItemType(t, itemTypeIf, lexer.NextItem())
+	assertItemType(t, itemTypeElse, lexer.NextItem())
+	assertItemType(t, itemTypeEnd, lexer.NextItem())
+	assertItemType(t, itemTypeFunction, lexer.NextItem())
+	assertItemType(t, itemTypeIdentifier, lexer.NextItem())
+}
+
+func assertItemType(t *testing.T, expected itemType, actualItem *Item) {
+	actual := actualItem.typ
+
+	if actual != expected {
+		t.Errorf("Item types not equal\n\t expected: \t%v\n\t   actual:\t%v <%v>",
+			expected, actual, actualItem)
+	}
+}
